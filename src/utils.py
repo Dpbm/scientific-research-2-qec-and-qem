6c82882328f6b1138c6345ce4de4ca9eeef7cac8
Dpbm <dpbm136@gmail.com>
2025-04-04 19:21:15 -0300

    testing mitigation level 3

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,13 @@
 import json
 import os
 
-def export_data_json(data:dict, filename:str):
-    with open(filename, "w") as file:
+def export_data_json(data, filename):
+    with open(filename, "w", encoding="utf-8") as file:
         print(f"saving {filename}...")
         json_obj = json.dumps(data, indent=4)
         file.write(json_obj)
 
-def generate_output_dir(path:str):
+def generate_output_dir(path):
     if(os.path.exists(path)):
         print(f"{path} already exists!")
         return
@@ -15,5 +15,10 @@ def generate_output_dir(path:str):
     print(f"Creating folder: {path}")
     os.mkdir(path)
 
-def generate_full_file_path(base_path:str, file:str) -> str:
+def generate_full_file_path(base_path, file):
     return os.path.join(base_path, file)
+
+def import_json_data(path):
+    print(f"Getting data from: {path}")
+    with open(path, 'r', encoding='utf-8') as file:
+        return json.load(file)
